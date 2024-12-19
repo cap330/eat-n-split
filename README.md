815c8f371e147f2a18065110efaaa209ad68cdc8
Dmitry Kazantsev <dmitry_kazantsev@yahoo.com>
2024-12-19 23:26:07 +0300

    097 Adding a New Friend

--- README.md
+++ README.md
@@ -5,3 +5,14 @@
 095 Building the Static App Forms
 
 096 Displaying the New Friend Form
+
+097 Adding a New Friend
+
+        Random avatar
+        https://i.pravatar.cc/{size}
+        Add an identify then always get same image
+        https://i.pravatar.cc/150?u=a042581f4e29026704d
+        Direct image id
+        https://i.pravatar.cc/150?img=3
+
+        crypto.randomUUID()
--- src/App.jsx
+++ src/App.jsx
@@ -22,17 +22,23 @@ const initialFriends = [
 ];
 
 function App() {
+  const [friends, setFriends] = useState(initialFriends);
   const [showAddFriend, setShowAddFriend] = useState(false);
 
   function handleSetShowAddFriend() {
     setShowAddFriend(show => !show);
   }
 
+  function handleSetFriends(friend) {
+    setFriends(friends => [...friends, friend]);
+    setShowAddFriend(false);
+  }
+
   return (
     <div className="app">
       <div className="sidebar">
-        <FriendsList />
-        {showAddFriend && <FormAddFriend />}
+        <FriendsList friends={friends} />
+        {showAddFriend && <FormAddFriend onAddFriend={handleSetFriends} />}
         <Button onClick={handleSetShowAddFriend}>{showAddFriend ? 'Close' : 'Add friend'}</Button>
       </div>
       <FormSplitBill />
@@ -48,10 +54,10 @@ function Button({ children, onClick }) {
   );
 }
 
-function FriendsList() {
+function FriendsList({ friends }) {
   return (
     <ul>
-      {initialFriends.map(friend => (
+      {friends.map(friend => (
         <Friend friend={friend} key={friend.id} />
       ))}
     </ul>
@@ -80,14 +86,31 @@ function Friend({ friend }) {
   );
 }
 
-function FormAddFriend() {
+function FormAddFriend({ onAddFriend }) {
+  const [name, setName] = useState('');
+  const [image, setImage] = useState('https://i.pravatar.cc/48');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!name || !image) return;
+    //const id = crypto.randomUUID();
+    const id = Math.floor(Math.random() * 100) + 1;
+
+    const newFriend = { name, image: `${image}?img=${id}`, balance: 0, id };
+
+    onAddFriend(newFriend);
+
+    //console.log(newFriend);
+  }
+
   return (
-    <form className="form-add-friend">
+    <form className="form-add-friend" onSubmit={handleSubmit}>
       <label>👫 Friend name</label>
-      <input type="text" />
+      <input type="text" value={name} onChange={e => setName(e.target.value)} />
 
       <label>📸 Imgage URL</label>
-      <input type="text" />
+      <input type="text" value={image} onChange={e => setImage(e.target.value)} />
 
       <Button>Add</Button>
     </form>
@@ -106,7 +129,7 @@ function FormSplitBill() {
       <input type="text" />
 
       <label>🥹 X's expenses</label>
-      <input type="text" disabled="true" />
+      <input type="text" disabled={true} />
 
       <label>Who is paying the bill</label>
       <select name="" id="">
